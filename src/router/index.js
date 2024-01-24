aabeb73f83930ca99da391d7e764912e66ad8902
quoctienphamm <quoctienphamm@gmail.com>
2024-01-24 19:23:15 +0700

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -83,29 +83,28 @@ const router = createRouter({
   ]
 })
 // eslint-disable-next-line no-unused-vars
-router.beforeEach(async (to, from) => {
-  // eslint-disable-next-line no-unused-vars
-  const { fetchUser, logout } = authStore()
-  const auth_layout = to.meta.requiresAuth
-  if (authStore().getToken != '') {
-    // fetchUser()
-    //   .then((res) => {
-    //     if (to.path != '/' && auth_layout == 1 && Object.keys(res.data.data).length == 0) {
-    //       logout()
-    //       window.location.href = '/'
-    //     }
-    //     if (auth_layout == -1 && Object.keys(res.data.data).length > 0)
-    //       window.location.href = '/khach-hang'
-    //   })
-    //   // eslint-disable-next-line no-unused-vars
-    //   .catch((error) => {
-    //     logout()
-    //     redirectLogin(to, auth_layout)
-    //   })
-    // instead of having to check every route record with
-    // to.matched.some(record => record.meta.requiresAuth)
-  } else {
-    redirectLogin(to, auth_layout)
-  }
-})
+// router.beforeEach(async (to, from) => {
+//   const { fetchUser, logout } = authStore()
+//   const auth_layout = to.meta.requiresAuth
+//   if (authStore().getToken != '') {
+//     fetchUser()
+//       .then((res) => {
+//         if (to.path != '/' && auth_layout == 1 && Object.keys(res.data.data).length == 0) {
+//           logout()
+//           window.location.href = '/'
+//         }
+//         if (auth_layout == -1 && Object.keys(res.data.data).length > 0)
+//           window.location.href = '/khach-hang'
+//       })
+//       // eslint-disable-next-line no-unused-vars
+//       .catch((error) => {
+//         logout()
+//         redirectLogin(to, auth_layout)
+//       })
+//     // instead of having to check every route record with
+//     // to.matched.some(record => record.meta.requiresAuth)
+//   } else {
+//     redirectLogin(to, auth_layout)
+//   }
+// })
 export default router
