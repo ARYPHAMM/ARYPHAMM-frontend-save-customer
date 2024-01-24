d6a6ff250e7af3a9388526d106f3631956be4a38
quoctienphamm <quoctienphamm@gmail.com>
2024-01-24 19:05:22 +0700

    fix deploy

--- src/main.js
+++ src/main.js
@@ -16,6 +16,38 @@ import '@/assets/scss/main.scss'
 // import 'bootstrap/dist/js/bootstrap.min.js'
 
 
+import "./assets/theme/libs/jquery/dist/jquery.min.js"
+
+import "./assets/theme/libs/bootstrap/dist/js/bootstrap.bundle.min.js"
+
+import "./assets/theme/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"
+
+import "./assets/theme/extra-libs/sparkline/sparkline.js"
+
+import "./assets/theme/dist/js/waves.js"
+
+import "./assets/theme/dist/js/sidebarmenu.js"
+
+import "./assets/theme/dist/js/custom.min.js"
+
+import "./assets/theme/libs/flot/excanvas.js"
+
+import "./assets/theme/libs/flot/jquery.flot.js"
+
+import "./assets/theme/libs/flot/jquery.flot.pie.js"
+
+import "./assets/theme/libs/flot/jquery.flot.time.js"
+
+import "./assets/theme/libs/flot/jquery.flot.stack.js"
+
+import "./assets/theme/libs/flot/jquery.flot.crosshair.js"
+
+import "./assets/theme/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"
+
+import "./assets/theme/dist/js/pages/chart/chart-page-init.js"
+
+
+
 // eslint-disable-next-line no-unused-vars
 import Popper from 'popper.js'
 const pinia = createPinia()
